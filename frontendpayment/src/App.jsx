bfe6e601fe17a0f38e3b71a61ce56de2bf848c86
AtanuG123 <109225904+AtanuG123@users.noreply.github.com>
2023-11-03 22:58:18 +0530

    nothing to do

--- frontendpayment/src/App.jsx
+++ frontendpayment/src/App.jsx
@@ -1,40 +1,26 @@
-import "./App.css";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Cart from "./component/Cart";
-import Fail from "./component/fail";
-import Success from "./component/test";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Success />}>
-      <Route index element={<Fail />} />
-      <Route path="/:id" element={<Cart />}></Route>
-      <Route path="/success" element={<Success />}></Route>
-      <Route path="/fail" element={<Fail />}></Route>
-    </Route>
-  )
-);
-function App({router}) {
-  // <BrowserRouter>
-  //   <Routes>
-  //     <Route path="/:id" element={<Cart />}></Route>
-  //     <Route path="/success" element={<Success />}></Route>
-  //     <Route path="/fail" element={<Fail />}></Route>
-  //   </Routes>
-  // </BrowserRouter>;
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './component/Cart';
+import Fail from './component/fail';
+import Success from './component/test';
+function App() {
+ 
+
   return (
     <>
-      <div className="App">
-        <RouterProvider router={router} />
+      <div className='App'>
+       <BrowserRouter>
+       <Routes>
+          <Route path="/:id" element={<Cart />}></Route>
+          <Route path="/success" element={<Success/>}></Route>
+          <Route path="/fail" element={<Fail/>}></Route>
+       </Routes>
+       </BrowserRouter>
       </div>
+      
     </>
   );
 }
 
-export default App;
+export default App
